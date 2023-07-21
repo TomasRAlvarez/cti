4e19f0ea1386407bd5dcf851ff6c25a4705c5b08
Tomas Alvarez <tomasralvarez@gmail.com>
2023-07-21 17:24:08 -0300

    Agregue las funcionalidades generales del prooyecto

--- main.js
+++ main.js
@@ -1,61 +1,128 @@
-function validar() {
-    let m2 = document.getElementById("cantidadM2").value;
-    let valorXm2 = document.getElementById("valorM2").value;
-    let operacionVenta = document.getElementById("venta").checked;
-    let operacionAlquiler = document.getElementById("alquiler").checked;
+//Asignacion de btns a variables
+const btnValorBarrio = document.getElementById("btnValorBarrio");
+const btnCotizar = document.getElementById("btnCotizar");
+const btnGuardar = document.getElementById("btnGuardar");
+const btnEliminar = document.getElementById("btnEliminar");
 
-    if (m2 == "" || valorXm2 == "") {
-        alert("Falta completar datos");
-        return false; //para validar en la funcion guardar
-    } else if (operacionVenta == false && operacionAlquiler == false) {
-        alert("Seleccionar una operacion");
-        return false; //para validar en la funcion guardar
-    } else if (operacionVenta == true && operacionAlquiler == true) {
-        alert("Seleccionar solo una operacion");
-        return false; //para validar en la funcion guardar
+//Funciones q ejecuta cada boton cuando se clickea
+btnValorBarrio.onclick = valorBarrio;
+btnCotizar.onclick = validar;
+btnGuardar.onclick = crearProp;
+btnEliminar.onclick = eliminar;
+
+//esta funcion es de ejemplo por ahora. Despues cada barrio va a tener un valor asignado y ese se le asignara "valorm2"
+function valorBarrio() {
+    document.getElementById("valorM2").value = 1000;
+}
+
+function validar() {
+    let nombreProp = document.getElementById("nombreProp").value;
+    let direcProp = document.getElementById("direcProp").value;
+    let valorXm2 = parseFloat(document.getElementById("valorM2").value);
+    let m2Totales = parseFloat(document.getElementById("m2Totales").value);
+    let m2Cubiertos = parseFloat(document.getElementById("m2Cubiertos").value);
+    let m2Descubiertos = parseFloat(document.getElementById("m2Descubiertos").value);
+    let m2Semicubiertos = parseFloat(document.getElementById("m2Semicubiertos").value);
+    //reviso que todos los campos necesarios esten completos
+    if (nombreProp === "" || direcProp === "" || valorXm2 === "" || m2Totales === "" || m2Cubiertos === "" || m2Semicubiertos === "" || m2Descubiertos === "") {
+        alert("Falta ingresar datos de la propiedad");
+    } else if (m2Totales != m2Cubiertos + m2Descubiertos + m2Semicubiertos) {
+        alert("Los metros cubiertos, descubiertos y semicubiertos no coinciden con los metros totales");
     } else {
-        cotizar(m2, valorXm2);
-        return true; //para validar en la funcion guardar
+        cotizar(valorXm2, m2Totales, m2Cubiertos, m2Descubiertos, m2Semicubiertos);
     }
 }
 
-function cotizar(m2, valorXm2) {
-    let valorTotal = m2 * valorXm2;
-    document.getElementById("valorFinal").value = valorTotal;
+//realizo los calculos necesarios para llegar al valor de la propiedad
+function cotizar(valorXm2, m2Totales, m2Cubiertos, m2Descubiertos, m2Semicubiertos) {
+    let m2Homogeneizados = m2Cubiertos + m2Semicubiertos * 0.6 + m2Descubiertos * 0.4;
+    let valorAprox = m2Homogeneizados * valorXm2;
+    let estadoProp = parseFloat(document.getElementById("estadoProp").value);
+    let valorFinal = valorAprox;
+
+    if (estadoProp < 20) {
+        valorFinal = valorAprox * 0.75;
+    } else if (estadoProp < 50) {
+        valorFinal = valorAprox * 0.9;
+    } else if (estadoProp > 70) {
+        valorFinal = valorAprox * 1.1;
+    }
+    document.getElementById("valorFinal").value = valorFinal.toFixed(2);
+    //habilito el boton para guardar la propiedad
+    btnGuardar.disabled = false;
+    return valorFinal, m2Totales, m2Cubiertos, m2Semicubiertos, m2Descubiertos;
 }
 
 function eliminar() {
     //borra todos los valores
-    document.getElementById("cantidadM2").value = "";
+    document.getElementById("nombreProp").value = "";
+    document.getElementById("direcProp").value = "";
     document.getElementById("valorM2").value = "";
-    document.getElementById("venta").checked = false;
-    document.getElementById("alquiler").checked = false;
+    document.getElementById("m2Totales").value = "";
+    document.getElementById("m2Cubiertos").value = "";
+    document.getElementById("m2Descubiertos").value = "";
+    document.getElementById("m2Semicubiertos").value = "";
+    document.getElementById("estadoProp").value = "50";
     document.getElementById("valorFinal").value = "";
+    document.getElementById("pileta").checked = false;
+    document.getElementById("parrilla").checked = false;
+    document.getElementById("jardin").checked = false;
+    document.getElementById("sum").checked = false;
+    document.getElementById("gimnasio").checked = false;
+    //deshabilito el boton para guardar la propiedad
+    btnGuardar.disabled = true;
 }
 
-let cantidadCotizaciones = 0; //declaro afuera de la funcion para que sea 0 al iniciar
-function guardar() {
-    let valorCotizacion = document.getElementById("valorFinal").value; //si esta vacia, validar() se ejecuta
-    if (validar() == true && valorCotizacion != "") {
-        cantidadCotizaciones = cantidadCotizaciones + 1;
-        console.log(cantidadCotizaciones);
-        alert("Su cotizacion se guardo correctamente");
-        eliminar();
-        contarCotizaciones(cantidadCotizaciones);
+//declaro array para guardar todas las propiedades
+let propsGuardadas = [];
+function crearProp() {
+    //Declaracion de todos los datos de la propiedad a cargar
+    let nombreProp = document.getElementById("nombreProp").value;
+    let direcProp = document.getElementById("direcProp").value;
+    let tipoCasa = document.getElementById("tipoCasa").checked;
+    let tipoDepto = document.getElementById("tipoDepto").checked;
+    let tipoPH = document.getElementById("tipoPH").checked;
+    let barrioProp = document.getElementById("barrioProp").value;
+    let estadoProp = document.getElementById("estadoProp").value;
+    let pileta = document.getElementById("pileta").checked;
+    let parrilla = document.getElementById("parrilla").checked;
+    let jardin = document.getElementById("jardin").checked;
+    let sum = document.getElementById("sum").checked;
+    let gimnasio = document.getElementById("gimnasio").checked;
+
+    //verifico que tipo de propiedad es
+    let tipoProp = "";
+    if (tipoCasa == true) {
+        tipoProp = "casa";
+    } else if (tipoDepto) {
+        tipoProp = "departamento";
+    } else if (tipoPH) {
+        tipoProp = "ph";
     }
-}
 
-//contador de cotizaciones con un for
-function contarCotizaciones(x) {
-    for (i = 0; i <= x; i++) {
-        if (i == x) {
-            switch (i) {
-                case 1:
-                    alert("Usted guardo: " + i + " cotizacion");
-                    break;
-                default:
-                    alert("Usted guardo: " + i + " cotizaciones");
-            }
-        }
+    //guardo la propiedad dentro del objeto
+    let propiedad = {
+        nombre: nombreProp,
+        direccion: direcProp,
+        barrio: barrioProp,
+        tipo: tipoProp,
+        valor: valorFinal.value,
+        metrosTotales: m2Totales.value,
+        metrosCubiertos: m2Cubiertos.value,
+        metrosSemincubiertos: m2Semicubiertos.value,
+        metrosDescubiertos: m2Descubiertos.value,
+        estado: estadoProp,
+        pileta: pileta,
+        parrilla: parrilla,
+        jardin: jardin,
+        sum: sum,
+        gimnasio: gimnasio,
+    };
+    propsGuardadas.push(propiedad);
+    alert("Su propiedad se guardo correctamente");
+    alert(`Usted guardo ${propsGuardadas.length} propiedades`);
+    //ciclo para ver en la consola las propiedades guardadas
+    for (let i = 0; i < propsGuardadas.length; i++) {
+        console.log(propsGuardadas[i]);
     }
 }
