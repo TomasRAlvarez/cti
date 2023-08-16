b95bd74587c681b26ea8162b96d322065bbe7b7c
Tomas Alvarez <tomasralvarez@gmail.com>
2023-08-16 15:20:49 -0300

    Finalizo prototipo de inicio de sesion. Posible para un solo usuario

--- js/login.js
+++ js/login.js
@@ -1,18 +1,39 @@
+const main = document.getElementById("mainLogin");
 const form = document.getElementById("loginForm");
 
-form.addEventListener("submit", (e) => {
-    const user = document.getElementById("user").value;
-    const password = document.getElementById("password").value;
-    e.preventDefault();
-    fetch("../json/users.json")
-        .then((data) => data.json())
-        .then((parsedData) => {
-            if (parsedData[user] != undefined) {
-                if (parsedData[user].password === password) {
-                    loginOk();
+userId = localStorage.getItem("id");
+console.log(userId);
+
+if (userId !== null) {
+    loginOk(userId);
+} else {
+    form.addEventListener("submit", (e) => {
+        const user = document.getElementById("user").value;
+        const password = document.getElementById("password").value;
+        e.preventDefault();
+        fetch("../json/users.json")
+            .then((data) => data.json())
+            .then((parsedData) => {
+                if (parsedData[user] != undefined) {
+                    if (parsedData[user].password === password) {
+                        localStorage.setItem("id", user);
+                        loginOk(user);
+                    } else {
+                        Toastify({
+                            text: "Contraseña Incorrecta",
+                            duration: 3000,
+                            close: true,
+                            stopOnFocus: false,
+                            gravity: "top",
+                            position: "right",
+                            style: {
+                                background: "linear-gradient(to right, #FF0707, #FF4E4E)",
+                            },
+                        }).showToast();
+                    }
                 } else {
                     Toastify({
-                        text: "Contraseña Incorrecta",
+                        text: "Usuario Incorrecto",
                         duration: 3000,
                         close: true,
                         stopOnFocus: false,
@@ -23,23 +44,20 @@ form.addEventListener("submit", (e) => {
                         },
                     }).showToast();
                 }
-            } else {
-                Toastify({
-                    text: "Usuario Incorrecto",
-                    duration: 3000,
-                    close: true,
-                    stopOnFocus: false,
-                    gravity: "top",
-                    position: "right",
-                    style: {
-                        background: "linear-gradient(to right, #FF0707, #FF4E4E)",
-                    },
-                }).showToast();
-            }
-        })
-        .catch((e) => "error", e);
-});
+            })
+            .catch((e) => "error", e);
+    });
+}
 
-function loginOk() {
-    console.log("Logueo");
+function loginOk(user) {
+    console.log(user);
+    main.innerHTML = `<section class="userPage">
+        <div  class="userPageTitle">
+            <h1>Bienvenido ${user}</h1>
+        </div>
+
+        <div  class="userPageStats"
+            <h2>Propiedades guardadas: ${localStorage.length - 1}</h2>
+        </div>
+    </section>`;
 }
